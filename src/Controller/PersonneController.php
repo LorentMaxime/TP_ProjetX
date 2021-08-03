ad0e375169a7a09349e13b3d1180e71466675e25
LorentMaxime <lorent.max@gmail.com>
2021-08-03 13:58:55 +0200

    UPDATE

---
+++ src/Controller/PersonneController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PersonneController extends AbstractController
+{
+
+   /**
+    * @Route("/", name="home")
+    */
+    public function home():Response
+    {
+        //$route = new Route()
+        return $this->render("personne/home.html.twig");
+    } 
+
+      /**
+    * @Route("/contact", name="contact")
+    */
+    public function contact():Response
+    {
+        //$route = new Route()
+        return $this->render("personne/contact.html.twig");
+    } 
+
+      /**
+    * @Route("/about-us", name="about")
+    */
+    public function about():Response
+    {
+        //$route = new Route()
+        return $this->render("personne/about.html.twig");
+    } 
+
+
+}
